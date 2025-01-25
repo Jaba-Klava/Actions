eae533c589d048ef649e1f4deebf0deb7f7946f5
Jaba-Klava <kristinasun55@gmail.com>
2025-01-25 19:21:48 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Main {
+     private Main() {
+        throw new UnsupportedOperationException("Этот класс не должен создаваться.");
+    }
+    
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
